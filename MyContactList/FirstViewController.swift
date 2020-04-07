5ea0f599f187f90453730dc1478cd2364c8331cf
Abdulhakim Darman <abdul10hakim@gmail.com>
2020-04-07 13:51:56 -0400

    Initial Commit

---
+++ MyContactList/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyContactList
+//
+//  Created by Abdulhakim Darman on 4/7/20.
+//  Copyright © 2020 Abdulhakim Darman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyContactList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyContactList
+//
+//  Created by Abdulhakim Darman on 4/7/20.
+//  Copyright © 2020 Abdulhakim Darman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
